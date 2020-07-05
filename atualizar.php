b0a35883e5e27bcb47174328206af047ff594bf7
CauanLima <65715709+CauanLima@users.noreply.github.com>
2020-07-05 16:42:43 -0300

    Delete atualizar.php

--- atualizar.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "usuarios.php";
-$v_usuario = new usuarios();
-$v_usuario->setNome($_POST['f_nome']);
-$v_usuario->setEmail($_POST['f_mail']);
-$v_usuario->update($_POST['f_id']);
-header("Location: cadastro.php");
-?>
